39ec3ed099756b1edb7ca3444a59ae5afa594a60
benstc <benstclair77@gmail.com>
2025-05-04 12:15:55 -0500

    initial commit

---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,17 @@
+import "./component_css/header.css"
+
+export default function Header() {
+
+    return (
+        <div className="header">
+            <p>Ben St. Clair</p>
+            <div className="header-right">
+                <p>About Me</p>
+                <p>Projects</p>
+                <p>Contact</p>
+                <p>LI</p>
+                <p>GH</p>
+            </div>
+        </div>
+    )
+}
